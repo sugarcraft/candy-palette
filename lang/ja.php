3ea0b02212cb06cbaafaf44343085c0f83a5ed4f
Joe Huss <detain@interserver.net>
2026-05-07 05:50:43 -0400

    Add Japanese locale for all libs (#254)

---
+++ lang/ja.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Japanese translations for candy-palette.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'standard.ansi16_out_of_range' => 'ANSI 16色インデックスは 0–15 である必要があります：{index}',
+];
