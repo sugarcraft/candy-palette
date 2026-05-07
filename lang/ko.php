c31d793a34bbea003548a3f1160c788b64246a46
Joe Huss <detain@interserver.net>
2026-05-07 05:56:41 -0400

    Add Korean locale for all libs (#257)

---
+++ lang/ko.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Korean translations for candy-palette.
+ *
+ * @return array<string, string>
+ */
+
+declare(strict_types=1);
+
+return [
+    'standard.ansi16_out_of_range' => 'ANSI 16색 인덱스는 0–15이어야 합니다: {index}',
+];
